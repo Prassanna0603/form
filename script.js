a2e30268bde5ad1b9184dbc1e6f5a6747d96ed3e
Prassanna0603 <prassanna@gmail.com>
2022-12-08 18:42:38 +0530

    commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+let button = document.getElementById("button")
+button.addEventListener("click",
+function registerForm(){
+    
+   var firstName = document.getElementById('firstname').value
+    console.log(firstName);
+    // let td1= document.getElementById("td1")
+    // td1.innerHTML=firstName
+
+    let lastName = document.getElementById('laststname').value
+    console.log(lastName);
+    let emailId = document.getElementById('email').value
+    console.log(emailId);
+    let address = document.getElementById('address').value
+    console.log(address)
+    let pineCode = document.getElementById('pincode').value
+    console.log(pineCode)
+ 3
+    let state = document.getElementById('state').value
+    console.log(state)
+    let country = document.getElementById('country').value
+    console.log(country)
+
+
+})
+
+
